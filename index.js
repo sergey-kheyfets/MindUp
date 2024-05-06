dc0638c7c77d9dceb42950335728fa431f8c66b6
Stepan_Kuchin <stepa.ku4in@gmail.com>
2024-05-06 23:40:14 +0500

    add center align

---
+++ index.js
@@ -0,0 +1,11 @@
+window.addEventListener('resize', () => {
+    const block = document.querySelector('.block');
+    // const blockWidth = Number(block.style.width);
+    // console.log(block.style.width)
+    const blockWidth = 400;
+    const screenWidth = window.innerWidth;
+    const colsCount = Math.floor(screenWidth * 0.9 / blockWidth);
+    console.log(colsCount)
+    const groups = document.querySelector('.groups');
+    groups.style.gridTemplateColumns = `repeat(${colsCount}, 1fr)`;
+})
