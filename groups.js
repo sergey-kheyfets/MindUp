ba5d69b6cfdddf0274805feb8bc5d10d010b5149
Stepan_Kuchin <stepa.ku4in@gmail.com>
2024-05-08 19:55:27 +0500

    красота в группах

--- groups.js
+++ groups.js
@@ -1,11 +1,11 @@
 function setGrid() {
-    const blocks = document.querySelector('.groups').querySelectorAll('.block');
+    const blocks = document.querySelector('.blocks').querySelectorAll('.block');
     const blockWidth = 300;
     const screenWidth = window.innerWidth;
     let colsCount = Math.floor(screenWidth * 0.9 / blockWidth);
     colsCount = Math.min(colsCount, blocks.length);
     colsCount = Math.max(colsCount, 1);
-    const groups = document.querySelector('.groups');
+    const groups = document.querySelector('.blocks');
     groups.style.gridTemplateColumns = `repeat(${colsCount}, 1fr)`;
 }
 
