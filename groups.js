dc46ace87b8169481619a5421b63798b5f6fe0d5
Stepan_Kuchin <stepa.ku4in@gmail.com>
2024-05-13 00:09:45 +0500

    try custom image-loader

--- groups.js
+++ groups.js
@@ -1,1 +1,16 @@
+const modalAdd = document.getElementById('modal-add');
+
+function openModal() {
+    modalAdd.style.opacity = 1;
+    modalAdd.style.pointerEvents = 'auto';
+}
+
+function closeModal() {
+    modalAdd.style.opacity = 0;
+    modalAdd.style.pointerEvents = 'none';
+}
+
+document.querySelector('.block.group.add').onclick = openModal;
+modalAdd.querySelector('.modal-blackout').onclick = closeModal;
+
 setGrid();
