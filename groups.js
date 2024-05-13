4ef70416da4964a2e3d2dc1cfe85ad4683e675e4
Stepan_Kuchin <stepa.ku4in@gmail.com>
2024-05-13 13:00:53 +0500

    modal add group done

--- groups.js
+++ groups.js
@@ -1,6 +1,7 @@
 const modalAdd = document.getElementById('modal-add');
 const imageLoad = document.getElementById('group-image-upload');
 const modalImageUpload = document.querySelector('.modal-view').querySelector('.custom-image-upload');
+const modalImageReloadText = modalImageUpload.querySelector('h3');
 
 function openModal() {
     modalAdd.style.opacity = 1;
@@ -10,6 +11,8 @@ function openModal() {
 function closeModal() {
     modalAdd.style.opacity = 0;
     modalAdd.style.pointerEvents = 'none';
+    setTimeout(() => {modalImageUpload.style.backgroundImage = 'url("site_images/upload_image.svg")';}, 100);
+    modalImageReloadText.style.opacity = 0;
 }
 
 document.querySelector('.block.group.add').onclick = openModal;
@@ -21,6 +24,7 @@ imageLoad.addEventListener('change', () => {
         const reader = new FileReader();
         reader.onload = (e) => {
             modalImageUpload.style.backgroundImage = `url(${e.target.result})`;
+            setTimeout(() => {modalImageReloadText.style.opacity = 1;}, 200);
         }
         reader.readAsDataURL(image);
     }
