71415fa0c762d2b1785ef1b91f6d478981de2e95
Stepan_Kuchin <stepa.ku4in@gmail.com>
2024-05-13 12:27:51 +0500

    upgrade image loader

--- groups.js
+++ groups.js
@@ -1,4 +1,6 @@
 const modalAdd = document.getElementById('modal-add');
+const imageLoad = document.getElementById('group-image-upload');
+const modalImageUpload = document.querySelector('.modal-view').querySelector('.custom-image-upload');
 
 function openModal() {
     modalAdd.style.opacity = 1;
@@ -12,5 +14,16 @@ function closeModal() {
 
 document.querySelector('.block.group.add').onclick = openModal;
 modalAdd.querySelector('.modal-blackout').onclick = closeModal;
+
+imageLoad.addEventListener('change', () => {
+    const image = imageLoad.files[0];
+    if (image) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            modalImageUpload.style.backgroundImage = `url(${e.target.result})`;
+        }
+        reader.readAsDataURL(image);
+    }
+});
 
 setGrid();
