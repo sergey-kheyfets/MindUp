659ffb8cac91bda114079827c74603a7e504cd0f
Stepan_Kuchin <stepa.ku4in@gmail.com>
2024-05-14 21:49:17 +0500

    fixed merge

---
+++ mindupback/mindup/static/backend_groups.js
@@ -0,0 +1,41 @@
+async function getGroups() {
+    const resp = await sendRequest('/mindup/api/all_groups');
+    return resp.result;
+}
+
+function createGroupHTML(groupJson) {
+    const author = groupJson['creator'];
+    const title = groupJson['title'];
+    const description = groupJson['description'];
+    const icon = groupJson['icon'];
+
+    const blockElement = document.createElement('div');
+    blockElement.classList.add('block');
+
+    const blockBackgroundElement = document.createElement('div');
+    blockBackgroundElement.classList.add('block-background');
+    if (icon !== '-') {
+        blockBackgroundElement.style.backgroundImage = `url('${icon}')`;
+    }
+
+
+    const h2Element = document.createElement('h2');
+    h2Element.innerHTML = title;
+
+    blockElement.appendChild(blockBackgroundElement);
+    blockElement.appendChild(h2Element);
+
+    return blockElement;
+}
+
+async function updateGroups() {
+    const result = await getGroups();
+    const blocks = document.querySelector('.blocks');
+    for (const group of result) {
+        const groupHTML = createGroupHTML(group);
+        blocks.appendChild(groupHTML);
+    }
+    setGrid();
+}
+
+updateGroups();
