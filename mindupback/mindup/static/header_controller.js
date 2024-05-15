c99000e7a6b8857cbf383cfd06749acd366205bd
Stepan_Kuchin <stepa.ku4in@gmail.com>
2024-05-15 15:45:31 +0500

    add profile html

--- mindupback/mindup/static/header_controller.js
+++ mindupback/mindup/static/header_controller.js
@@ -1,4 +1,6 @@
 const searchInput = document.getElementById('searchInput');
+const profileInfo = document.querySelector('.profile-info-container');
+let isProfileOpened = false;
 
 function search() {
     const text = searchInput.value;
@@ -13,3 +15,25 @@ function handleKeyPress(event) {
         search();
     }
 }
+
+function closeProfileInfo() {
+    profileInfo.style.opacity = 0;
+    profileInfo.style.pointerEvents = 'none';
+    isProfileOpened = false;
+}
+
+function openProfileInfo() {
+    if (isProfileOpened) {
+        closeProfileInfo();
+    } else {
+        profileInfo.style.opacity = 1;
+        profileInfo.style.pointerEvents = 'auto';
+        isProfileOpened = true;
+    }
+}
+
+document.addEventListener('click', function(event) {
+    if (!profileInfo.contains(event.target) && !event.target.closest('.menuGroup.profile')) {
+        closeProfileInfo();
+    }
+});
